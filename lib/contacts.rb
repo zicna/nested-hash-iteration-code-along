7b5e543e3464585fca2ad7a7e4dca8ddf9dfb3ec
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-13 13:45:54 -0600

    commit tree 7892b667814d6a5865b8137a7de56e821e64e1fe

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,29 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
+  contacts.each do |name, info|
+    #puts info
+    info.each do |attribute, value|
+      #puts attribute
+      if attribute == :favorite_ice_cream_flavors
+        #puts value
+        value.delete_if {|ice_cream| ice_cream == "strawberry"}
 
+      end
+    end
+  end
 end
+
+contacts = {
+    "Jon Snow" => {
+      name: "Jon",
+      email: "jon_snow@thewall.we", 
+      favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+    },
+    "Freddy Mercury" => {
+      name: "Freddy",
+      email: "freddy@mercury.com",
+      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+  }
+  remove_strawberry(contacts)
